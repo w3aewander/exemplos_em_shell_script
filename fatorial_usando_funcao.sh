9400b42ad6bc035935496a6a2738e150b2ba66a8
Wanderlei Silva do Carmo <wander.silva@gmail.com>
2015-02-21 21:25:37 -0200

    Update fatorial_usando_funcao.sh

--- fatorial_usando_funcao.sh
+++ fatorial_usando_funcao.sh
@@ -5,7 +5,29 @@
 #
 
 function fatorial() {
+   
+   #verifica se há parâmetos
+   #se  não houver parametros avise ao usuário
+   if [ -n $1 ]; then
+      echo "Não foi informado um inteiro para o cáclulo fatorial."
+      exit 0
+   fi   
+     
+   #verifica se há parâmetos
+   #se  não houver parametros avise ao usuário
+   if [ -e $1 ]; then
+      echo "Não foi informado um inteiro para o cáclulo fatorial."
+      exit 1
+   fi
 
+  #Testa para ver se é um inteiro válido
+  if [[ ! $1 = ?(+|-)+([0-9])  ]]; then
+      echo "Por favor, informe um número inteiro"
+      exit 1
+  fi
+
+   
+   #declara as variáveis e atribui o valor enviado como parametro à elas.
    let n=$1
    let fat=1
 
